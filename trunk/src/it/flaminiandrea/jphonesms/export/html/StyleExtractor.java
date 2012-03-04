5c6b41753c71a0bf48827d0338faa4879d77f5d1
and.flamini <and.flamini@20c4d3da-4aba-11de-9ab4-498524d4d007>
2012-03-04 18:23:08 +0000

    minor changes

--- trunk/src/it/flaminiandrea/jphonesms/export/html/StyleExtractor.java
+++ trunk/src/it/flaminiandrea/jphonesms/export/html/StyleExtractor.java
@@ -5,22 +5,19 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class StyleExtractor {
 
-	@SuppressWarnings("unchecked")
 	public static boolean unzip(String fileUri, String pathWhereToExtract) throws IOException {
 		final int BUFFER = 2048;
 		BufferedOutputStream dest = null;
 		BufferedInputStream is = null;
 		ZipEntry entry;
 		ZipFile zipfile = new ZipFile(fileUri);
-		Enumeration e = zipfile.entries();
-		while(e.hasMoreElements()) {
-			entry = (ZipEntry) e.nextElement();
+		while(zipfile.entries().hasMoreElements()) {
+			entry = (ZipEntry) zipfile.entries().nextElement();
 			is = new BufferedInputStream(zipfile.getInputStream(entry));
 			int count;
 			byte data[] = new byte[BUFFER];
